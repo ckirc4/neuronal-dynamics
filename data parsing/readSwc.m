9b223f585ab2b80296b20bd86b07d297aea1a597
ckirc4 <ckirc4@github.com>
2017-11-26 14:20:28 +1000

    readSwc error and readability
    Instead of showing a warning when a file cannot be opened, the program now stops

--- data parsing/readSwc.m
+++ data parsing/readSwc.m
@@ -2,25 +2,12 @@ function output = readSwc(file)
 % reads the SWC file and outputs it as a n*7 matrix
 
 %% Find and open file
-if nargin == 1 % file specified
-    % type 1 = file, type 2 = url
-    type = validateParameters(file);
-    [id, success] = openFile(file, type);
+if nargin == 0
+    file = openDialog(); % returns full file location
 end
 
-if nargin == 0 || ~success
-    [file, filePath] = openDialog();
-    try
-        validateParameters(file, 1);
-        [id, success] = openFile([filePath file]);
-    catch
-        success = 0; 
-    end
-    if ~success
-        error('The file could not be opened');
-    end
-    fprintf('readSwc(''%s'')\n',file);
-end
+type = validateParameters(file);
+id = openFile(file, type);
 
 %% Find data
 reachedData = false;
@@ -52,7 +39,7 @@ reachedEnd = false;
 while ~reachedEnd
     % check if end is reached
     if isequal(thisLine,-1) || isempty(thisLine)
-        reachedEnd = true; 
+        reachedEnd = true;
         continue;
     end
     
@@ -88,22 +75,24 @@ end
 
 end
 
+function fileID = openFile(file, type)
 
-function [fileID, success] = openFile(name, type)
-
-success = true;
-if type == 2
-    url = name;
-    name = strcat(num2str(floor(rand(10,1)*10))','.swc');
-websave(strcat('Temp/',name),url)
+if type == 'url'
+    % save website to temporary .swc file
+    url = file;
+    file_temp = 'temp.swc';
+    file = websave(file_temp,url);
 end
-fileID = fopen(name);
+
+fileID = fopen(file);
+delete(file_temp);
+
 if (fileID == -1)
-    warning('Specified file could not be found')
-    success = false;
+    error('Specified file could not be found.')
 end
 
 end
+
 function [file, path] = openDialog()
 
 [file, path] = uigetfile('.swc','Select .swc file');
@@ -113,13 +102,15 @@ if isequal(file,0) || isequal(path,0)
 end
 
 end
+
 function v = parseLine(line)
 
-    v = str2num(line);  %#ok<ST2NM>
-    % for large data sets, may have to modify this by converting the 1st,
-    % 2nd and last columns into uint types to save memory
-    
+v = str2num(line);  %#ok<ST2NM>
+% for large data sets, may have to modify this by converting the 1st,
+% 2nd and last columns into uint types to save memory
+
 end
+
 function verifyLineVector(v,k)
 
 if any(isnan(v)) || isempty(v)
