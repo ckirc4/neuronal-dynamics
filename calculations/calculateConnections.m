cec57310885795e89b4a50b6e68f4ea17b161f24
ckirc4 <30758803+ckirc4@users.noreply.github.com>
2017-11-27 15:35:55 +1000

    cleanup
    Renamed variables, added lots more comments, reformated code slightly to make it more logical

--- calculations/calculateConnections.m
+++ calculations/calculateConnections.m
@@ -2,40 +2,47 @@ function [compartments, nodes] = calculateConnections(data)
 % Calculates two arrays that directly relate the nodes and compartments
 % (both ways).
 %
-% compartments   (nNodes-1)*2 array 
+% compartments   (nNodes-1)*2 array
 %       Which nodes (columns) the compartments (rows) connect
 %       Each compartment must connect two nodes
 % nodes:         nNodes*x array
 %       Which compartments (columns) the nodes (rows) are connected by
-%       Each node is connected by at least one compartment 
+%       Each node is connected by at least one compartment
 %       (1 if the node is at an endpoint)
 
-m = size(data,1);
+nNodes = size(data,1);
 
-cC = zeros(m-1,2); % which two points connect each compartment?
-cP = zeros(m,3); % which compartments does each point connect?
+compartments = zeros(nNodes-1,2);
+nodes = zeros(nNodes,3); % for now, assume largest junction consists of three compartments
 
-for i = 1:m-1 % i are the compartment numbers, i.e. i+1'th row of the data is the i'th compartment
-    % for cC
-    cC(i,1) = i+1; % k'th compartment connected by k+1'th point
-    cC(i,2) = data(i+1,7); % other point is the parent
+for i = 1:nNodes-1
     
-    % for cP
-    P = cC(i,1:2); % the two points that the i'th compartment connects to
+    % The data rows correspond to the nodes
+    % Compartment #1 connects nodes 1 and 2
+    % Compartment #i connects nodes x and i+1 (x is the parent)
+    compartments(i,2) = i+1;
+    compartments(i,1) = data(i+1,7);
     
-    numC = size(cP,2);
-    done = [0 0]; % whether the two P values have been filled into cP yet
-    for c = 1:numC % columns
-        for p = 1:2
-            thisP = P(p);
-            if cP(thisP,c) == 0
-                if ~done(p)
-                    cP(thisP,c) = i;
-                    done(p) = 1;
+    N = compartments(i,1:2); % the two nodes that the i'th compartment connects
+    
+    maxCompartments = size(nodes,2);
+    
+    % We now want to assign the two nodes in the N vector to their
+    % corresponding positions in the nodes array
+    assigned = [false false]; % whether each of the nodes has been assigned yet
+    for col = 1:maxCompartments
+        for nodeIndex = 1:2 % first or second node in N
+            
+            if ~assigned(nodeIndex)
+                thisNode = N(nodeIndex); % the node number we are considering now
+                if nodes(thisNode,col) == 0 % whether the relevant position in the nodes array is occupied yet
+                    nodes(thisNode,col) = i; % this node is connected by the i'th compartment
+                elseif (col == maxCompartments && nodes(thisNode,col) ~= 0) % have reached the max column, but this node hasn't been assigned yet
+                    nodes(thisNode,col+1) = i; % this row of cP is filled up, so extend it
                 end
-            elseif c == numC && cP(thisP,c) ~= 0
-                cP(thisP,c+1) = i; % this row of cP is filled up, so extend it
+                assigned(nodeIndex) = true;
             end
+            
         end
     end
 end
