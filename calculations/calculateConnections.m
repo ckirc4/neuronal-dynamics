072b9a887d5a13d45817bdc002280f22d490a838
ckirc4 <30758803+ckirc4@users.noreply.github.com>
2017-12-05 14:29:10 +1000

    better documentation, soma node counting

--- calculations/calculateConnections.m
+++ calculations/calculateConnections.m
@@ -1,31 +1,42 @@
-function [compartments, nodes] = calculateConnections(data)
+function [compartmentList, nodeList] = calculateConnections(data)
 % Calculates two arrays that directly relate the nodes and compartments
 % (both ways).
 %
-% compartments   (nNodes-1)*2 array
-%       Which nodes (columns) the compartments (rows) connect
-%       Each compartment must connect two nodes
-% nodes:         nNodes*x array
-%       Which compartments (columns) the nodes (rows) are connected by
-%       Each node is connected by at least one compartment
-%       (1 if the node is at an endpoint)
+% compartmentList   (nNodes-(nSomaNodes-1)*2 array
+%       A list of compartments (rows) and which nodes (columns) they
+%       connect. 
+%       Each compartment must connect two nodes.
+% nodeList:         nNodes*(maxConnections)+1 array
+%       A list of nodes (rows) and which compartments (columns) they are 
+%       connected by.
+%       Each node is connected by at least one compartment.
+%       The last column counts the number of connections (number of nonzero
+%       entries per row).
 
-nNodes = size(data,1);
+nNodes = size(data,1); % number of rows
+nSomaNodes = 0;
 
-compartments = zeros(nNodes-1,2);
-nodes = zeros(nNodes,3); % for now, assume largest junction consists of three compartments
+% count the number of nodes defining the soma
+for thisNode = 1:nNodes
+    if (data(thisNode,2) == 1) % soma
+        nSomaNodes = nSomaNodes + 1;
+    end
+end
+
+compartmentList = zeros(nNodes-(nSomaNodes-1),2);
+nodeList = zeros(nNodes,3); % for now, assume largest junction consists of two compartments
 
 for i = 1:nNodes-1
     
     % The data rows correspond to the nodes
     % Compartment #1 connects nodes 1 and 2
     % Compartment #i connects nodes x and i+1 (x is the parent)
-    compartments(i,2) = i+1;
-    compartments(i,1) = data(i+1,7);
+    compartmentList(i,2) = i+1;
+    compartmentList(i,1) = data(i+1,7);
     
-    N = compartments(i,1:2); % the two nodes that the i'th compartment connects
+    N = compartmentList(i,1:2); % the two nodes that the i'th compartment connects
     
-    maxCompartments = size(nodes,2);
+    maxCompartments = size(nodeList,2);
     
     % We now want to assign the two nodes in the N vector to their
     % corresponding positions in the nodes array
@@ -35,11 +46,11 @@ for i = 1:nNodes-1
             
             if ~assigned(nodeIndex)
                 thisNode = N(nodeIndex); % the node number we are considering now
-                if nodes(thisNode,col) == 0 % whether the relevant position in the nodes array is occupied yet
-                    nodes(thisNode,col) = i; % this node is connected by the i'th compartment
+                if nodeList(thisNode,col) == 0 % whether the relevant position in the nodes array is occupied yet
+                    nodeList(thisNode,col) = i; % this node is connected by the i'th compartment
                     assigned(nodeIndex) = true;
-                elseif (col == maxCompartments && nodes(thisNode,col) ~= 0) % have reached the max column, but this node hasn't been assigned yet
-                    nodes(thisNode,col+1) = i; % this row of cP is filled up, so extend it
+                elseif (col == maxCompartments && nodeList(thisNode,col) ~= 0) % have reached the max column, but this node hasn't been assigned yet
+                    nodeList(thisNode,col+1) = i; % this row of cP is filled up, so extend it
                     assigned(nodeIndex) = true;
                 end
             end
